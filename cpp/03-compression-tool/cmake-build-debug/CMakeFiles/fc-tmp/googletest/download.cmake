fe4181808eb4ea165c716c269b631139ab5a939f
pedrobarbeira <pedro.barbeira95@gmail.com>
2025-04-09 13:48:34 +0100

    Fixed file structure

---
+++ cpp/03-compression-tool/cmake-build-debug/CMakeFiles/fc-tmp/googletest/download.cmake
@@ -0,0 +1,12 @@
+cmake_minimum_required(VERSION 3.29)
+
+message(VERBOSE "Executing download step for googletest")
+
+block(SCOPE_FOR VARIABLES)
+
+include("/home/pbarbeira/dev/code/john-crickett-weekly/cpp/huffman-encoding/cmake-build-debug/CMakeFiles/fc-stamp/googletest/download-googletest.cmake")
+include("/home/pbarbeira/dev/code/john-crickett-weekly/cpp/huffman-encoding/cmake-build-debug/CMakeFiles/fc-stamp/googletest/verify-googletest.cmake")
+include("/home/pbarbeira/dev/code/john-crickett-weekly/cpp/huffman-encoding/cmake-build-debug/CMakeFiles/fc-stamp/googletest/extract-googletest.cmake")
+
+
+endblock()
---
+++ cpp/03-compression-tool/cmake-build-debug/_deps/googletest-build/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/pbarbeira/dev/code/john-crickett-weekly/cpp/huffman-encoding/cmake-build-debug/_deps/googletest-src/googletest
+# Build directory: /home/pbarbeira/dev/code/john-crickett-weekly/cpp/huffman-encoding/cmake-build-debug/_deps/googletest-build/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
